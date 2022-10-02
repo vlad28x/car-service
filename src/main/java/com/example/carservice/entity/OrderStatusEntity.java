f05594ebe072cbcc433e02140995c79320eaa4c9
Anastasia7868 <nastya7868@gmail.com>
2022-10-02 22:14:33 +0300

    TASK-1: Add @Table and @Column in Entity

--- src/main/java/com/example/carservice/entity/OrderStatusEntity.java
+++ src/main/java/com/example/carservice/entity/OrderStatusEntity.java
@@ -1,13 +1,17 @@
 package com.example.carservice.entity;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.OneToMany;
+import javax.persistence.Table;
 import java.util.ArrayList;
 import java.util.List;
 
 @Entity
+@Table(name = "order_status")
 public class OrderStatusEntity extends BaseEntity<Long> {
 
+    @Column(name = "name")
     private String name;
     @OneToMany(mappedBy = "orderStatus")
     private List<OrderEntity> orders = new ArrayList<>();
