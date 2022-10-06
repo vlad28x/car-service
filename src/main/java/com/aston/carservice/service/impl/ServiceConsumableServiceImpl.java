e5a655864dcc8de092a39e4c2b3401782f0185c8
Dmitry Valevach <Valevachdima23@gmail.com>
2022-10-06 23:50:35 +0300

    CAR-17:Redesign services to accommodate new components

--- src/main/java/com/aston/carservice/service/impl/ServiceConsumableServiceImpl.java
+++ src/main/java/com/aston/carservice/service/impl/ServiceConsumableServiceImpl.java
@@ -2,16 +2,16 @@ package com.aston.carservice.service.impl;
 
 import com.aston.carservice.dto.ServiceConsumableRequestDto;
 import com.aston.carservice.dto.ServiceConsumableResponseDto;
-import com.aston.carservice.entity.ServiceConsumableEntity;
 import com.aston.carservice.entity.ServiceConsumableId;
 import com.aston.carservice.exception.NotFoundException;
 import com.aston.carservice.repositories.ServiceConsumableRepository;
 import com.aston.carservice.service.ServiceConsumableService;
-import com.aston.carservice.util.mapper.ServiceConsumableMapper;
+import com.aston.carservice.util.mapper.newmapper.ServiceConsumableMapper;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 
@@ -20,52 +20,58 @@ import java.util.stream.Collectors;
 public class ServiceConsumableServiceImpl implements ServiceConsumableService {
 
     private final ServiceConsumableRepository serviceConsumableRepository;
+    private final ServiceConsumableMapper serviceConsumableMapper;
 
-    public ServiceConsumableServiceImpl(ServiceConsumableRepository serviceConsumableRepository) {
+    public ServiceConsumableServiceImpl(ServiceConsumableRepository serviceConsumableRepository, ServiceConsumableMapper serviceConsumableMapper) {
         this.serviceConsumableRepository = serviceConsumableRepository;
+        this.serviceConsumableMapper = serviceConsumableMapper;
     }
 
     @Override
     public ServiceConsumableResponseDto getById(ServiceConsumableId id) {
-        return ServiceConsumableMapper.userEntityToServiceConsumableResponseDto(
-                serviceConsumableRepository.findById(id).orElseThrow(() ->
-                        new NotFoundException(String.format("ServiceConsumable with ID %s not found", id)))
-        );
+        return serviceConsumableRepository.findById(id)
+                .map(serviceConsumableMapper::toResponseDto)
+                .orElseThrow(() -> new NotFoundException(String
+                        .format("ServiceConsumable with ID %s not found", id)));
     }
 
     @Override
     public List<ServiceConsumableResponseDto> getAll() {
-        return serviceConsumableRepository
-                .findAll()
-                .stream()
-                .map(ServiceConsumableMapper::userEntityToServiceConsumableResponseDto)
+        return serviceConsumableRepository.findAll().stream()
+                .map(serviceConsumableMapper::toResponseDto)
                 .collect(Collectors.toList());
     }
 
     @Override
     @Transactional
     public ServiceConsumableResponseDto create(ServiceConsumableRequestDto newServiceConsumable) {
-        return ServiceConsumableMapper.userEntityToServiceConsumableResponseDto(
-                serviceConsumableRepository.save(ServiceConsumableMapper
-                        .serviceConsumableRequestDtoToServiceConsumableEntity(newServiceConsumable))
-        );
+        return Optional.ofNullable(newServiceConsumable)
+                .map(serviceConsumableMapper::toEntity)
+                .map(serviceConsumableRepository::save)
+                .map(serviceConsumableMapper::toResponseDto)
+                .orElse(null);
     }
 
     @Override
     @Transactional
     public ServiceConsumableResponseDto update(ServiceConsumableId id,
                                                ServiceConsumableRequestDto newServiceConsumable) {
-        ServiceConsumableEntity serviceConsumableEntity = serviceConsumableRepository.findById(id).orElseThrow(() ->
-                new NotFoundException(String.format("ServiceConsumable with ID %s not found", id)));
-        serviceConsumableEntity.setCount(newServiceConsumable.getCount());
-        return ServiceConsumableMapper.userEntityToServiceConsumableResponseDto(serviceConsumableEntity);
+        return serviceConsumableRepository.findById(id)
+                .map(entity -> serviceConsumableMapper.toEntity(newServiceConsumable, entity))
+                .map(serviceConsumableRepository::saveAndFlush)
+                .map(serviceConsumableMapper::toResponseDto)
+                .orElseThrow(() -> new NotFoundException(String.format("ServiceConsumable with ID %s not found", id)));
     }
 
     @Override
     @Transactional
     public boolean delete(ServiceConsumableId id) {
-        serviceConsumableRepository.deleteById(id);
-        return true;
+        return serviceConsumableRepository.findById(id)
+                .map(entity -> {
+                    serviceConsumableRepository.delete(entity);
+                    return true;
+                })
+                .orElseThrow(() -> new NotFoundException(String.format("ServiceConsumable with ID %s not found", id)));
     }
 
 }
