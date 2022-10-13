695e4669c96f78e49df86e33235667fb335382eb
vlad28x <vlad.skibin@mail.ru>
2022-10-13 19:16:23 +0300

    CAR-36: Get all orders for current Customer

--- src/main/java/com/aston/carservice/repository/OrderRepository.java
+++ src/main/java/com/aston/carservice/repository/OrderRepository.java
@@ -4,6 +4,11 @@ import com.aston.carservice.entity.OrderEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface OrderRepository extends JpaRepository<OrderEntity, Long> {
+
+    List<OrderEntity> findAllByCustomerUsername(String username);
+
 }
--- src/main/java/com/aston/carservice/service/OrderService.java
+++ src/main/java/com/aston/carservice/service/OrderService.java
@@ -3,5 +3,11 @@ package com.aston.carservice.service;
 import com.aston.carservice.dto.OrderRequestDto;
 import com.aston.carservice.dto.OrderResponseDto;
 
+import java.security.Principal;
+import java.util.List;
+
 public interface OrderService extends Service<OrderResponseDto, OrderRequestDto, Long> {
+
+    List<OrderResponseDto> getAllOrdersCurrentCustomer(Principal principal);
+
 }
